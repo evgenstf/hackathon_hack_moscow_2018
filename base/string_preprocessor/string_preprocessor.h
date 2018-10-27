56b773865fe5064a0eac717539938111cd03465a
dima-zakharov@ <dimaz1301@gmail.com>
2018-10-27 22:08:59 +0300

    move implemenation to cpp file

--- base/string_preprocessor/string_preprocessor.h
+++ base/string_preprocessor/string_preprocessor.h
@@ -6,53 +6,15 @@
 
 namespace preprocessing {
 
-namespace {
-  std::string to_lower(std::string s) {
-    for (char& c : s) {
-      c = std::tolower(c);
-    }
-    return s;
-  }
-}
-
 class StringPreprocessor {
 public:
-  StringPreprocessor()
-  {
-  }
+  StringPreprocessor();
 
-  void set_forbidden_chars(const std::string& chars) {
-    chars_ = to_lower(chars);
-  }
+  void set_forbidden_chars(const std::string& chars);
 
-  void set_forbidden_words(const std::vector<std::string>& words) {
-    words_ = words;
-    for (std::string& word : words_) {
-      word = to_lower(word);
-    }
-  }
+  void set_forbidden_words(const std::vector<std::string>& words);
 
-  std::string preprocess(std::string original) const {
-    original = to_lower(original);
-    std::string result;
-    for (size_t i = 0; i < original.size(); ++i) {
-      bool match_any_word = false;
-      for (const std::string& word : words_) {
-        if (original.substr(i, word.size()) == word) {
-          i += word.size() - 1;
-          match_any_word = true;
-          break;
-        }
-      }
-      if (match_any_word) {
-        continue;
-      }
-      if (chars_.find(original[i]) == std::string::npos)
-        continue;
-      result += original[i];
-    }
-    return result;
-  }
+  std::string preprocess(std::string original) const;
 
 private:
   std::string chars_;
