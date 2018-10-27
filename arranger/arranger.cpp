16e13d03e80f314944f3ac113c5205cfaab0dbba
Hippskill <hippskill@yandex-team.ru>
2018-10-27 20:01:29 +0300

    new formula

--- arranger/arranger.cpp
+++ arranger/arranger.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <cmath>
 
 namespace {
 
@@ -75,7 +76,7 @@ double Arranger::GetScore(const PredictionSet& prediction_set) const {
             }
         }
 
-        score += 1 / (max_product - min_product + 1);
+        score += sqrt(max_product) / (max_product - min_product + 1);
     }
 
     return score * companies_by_cluster.size();
