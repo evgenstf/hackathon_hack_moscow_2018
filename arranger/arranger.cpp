4bdbc3e75cdf12b5aac8e1a17c2c9a663d8d2fe8
Hippskill <hippskill@yandex-team.ru>
2018-10-27 19:40:55 +0300

    better formula

--- arranger/arranger.cpp
+++ arranger/arranger.cpp
@@ -75,7 +75,7 @@ double Arranger::GetScore(const PredictionSet& prediction_set) const {
             }
         }
 
-        score += 1 / (max_product - min_product + 1e-9);
+        score += 1 / (max_product - min_product + 1);
     }
 
     return score * companies_by_cluster.size();
