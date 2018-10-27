bb03dd21ea233282c3a613f50f111b75f0f37a57
dima-zakharov@ <dimaz1301@gmail.com>
2018-10-27 18:17:28 +0300

    dbscan: fix build

--- base/dbscan/dbscan.h
+++ base/dbscan/dbscan.h
@@ -2,7 +2,6 @@
 
 #include "point.h"
 
-#include <mutex>
 #include <vector>
 
 enum class EClassified {
@@ -20,10 +19,10 @@ public:
   {
   }
 
-  const std::vector<unsigned int>& get_clusters() const {
-    try {
-      std::call_once(flag, run);
-    } catch (...) {
+  const std::vector<unsigned int>& get_clusters() {
+    if (!processed_) {
+      run();
+      processed_ = true;
     }
     return cluster_;
   }
@@ -35,7 +34,7 @@ private:
     classified_.resize(points_.size(), EClassified::NOT_CLASSIFIED);
     cluster_.resize(points_.size());
     for (size_t i = 0; i < points_.size(); ++i) {
-      if (classified[i] != EClassified::NOT_CLASSIFIED)
+      if (classified_[i] != EClassified::NOT_CLASSIFIED)
         continue;
 
       if (is_core_object(i)) {
@@ -58,8 +57,8 @@ private:
     if (!is_core_object(i))
       return;
 
-    for (size_t j : adjucent_points_[i]) {
-      if (classified_[j] != EClassfied::NOT_CLASSIFIED)
+    for (size_t j : adjacent_points_[i]) {
+      if (classified_[j] != EClassified::NOT_CLASSIFIED)
         continue;
       dfs(j, cluster);
     }
@@ -72,7 +71,7 @@ private:
         if (i == j)
           continue;
 
-        if (points_[i].distance_to(points_[j]) <= eps) {
+        if (points_[i].distance_to(points_[j]) <= eps_) {
           adjacent_points_[i].push_back(j);
         }
       }
@@ -91,6 +90,5 @@ private:
   std::vector<EClassified> classified_;
   std::vector<unsigned int> cluster_;
   size_t clusters_num_ = 0;
-
-  std::once_flag flag;
-}
+  bool processed_ = false;
+};
